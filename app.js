7366e3063781d812a5d89b0b1eb92da32299ad0e
Mateusinho Dev <mateusdev23@gmail.com>
2023-05-24 11:16:49 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando sistema de gerencimaento de jogos")
 console.log("Aplicativo está se conectando ao banco de dados")
 // mais um console de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o Github")
